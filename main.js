9cd681470152749f367845ddb929c12b4207e8a5
alessandro <alessandrohidalgo8@gmail.com>
2024-11-05 18:40:24 -0500

    se agrego el proyecto

---
+++ main.js
@@ -0,0 +1,132 @@
+
+let propiedad = [
+    {
+        foto:"https://photos.zillowstatic.com/fp/fbd8b9e601aa2ffdc94ab3dbc00e793b-cc_ft_768.webp",
+        foto2:"https://photos.zillowstatic.com/fp/acf513515009d69a6b663f5c399cf033-cc_ft_384.webp",
+        foto3:"https://photos.zillowstatic.com/fp/0c614d66c2808a327e69016cbbbff094-cc_ft_384.webp",
+        foto4:"https://photos.zillowstatic.com/fp/799f290ba5d5fca225a459f14dde4773-cc_ft_384.webp",
+        foto5:"https://photos.zillowstatic.com/fp/d7f93043bddbc8607a6669134ab53826-cc_ft_384.webp",
+        precio:"$500,000",
+        direccion:"15208 SW 112th Pl, Miami, FL 33157",
+        cuartos:3,
+        baños:3,
+        piesCuadrados:"1,234",
+    },
+    {
+        foto:"https://photos.zillowstatic.com/fp/c0382e2b700d6e8fae4af71dc0ce92cf-p_e.webp",
+        foto2:"https://photos.zillowstatic.com/fp/d69115320e661993a85aa42d6881a1a5-cc_ft_384.webp",
+        foto3:"https://photos.zillowstatic.com/fp/be9f067adecd94a52a6839ed91fc8af3-cc_ft_384.webp",
+        foto4:"https://photos.zillowstatic.com/fp/f7bfdefb8377183875f59019b08a4fec-cc_ft_384.webp",
+        foto5:"https://photos.zillowstatic.com/fp/ccd21b9cd4fa20f5fc94d712094bbb82-cc_ft_384.webp",
+        precio:"$220,000",
+        direccion:"17111 SW 115th Ave, Miami, FL 33157",
+        cuartos:5,
+        baños:2,
+        piesCuadrados:"1,341",
+    },
+    {
+        foto:"https://photos.zillowstatic.com/fp/18a8b0ec5d156b160d2b886a1d6f7cdd-p_e.webp",
+        foto2:"https://photos.zillowstatic.com/fp/466fe3d4a657a3b6a215648ce2d93cbb-cc_ft_384.webp",
+        foto3:"https://photos.zillowstatic.com/fp/f5f744d6291c8d8b506e2f64b680531e-cc_ft_384.webp",
+        foto4:"https://photos.zillowstatic.com/fp/0fe3e74aab0b045fd77d233ac1eea0bb-cc_ft_384.webp",
+        foto5:"https://photos.zillowstatic.com/fp/ca13f4a3211f03a05bbf59b18372e70d-cc_ft_384.webp",
+        precio:"$990,000",
+        direccion:"10740 SW 155th St, Miami, FL 33157",
+        cuartos:8,
+        baños:6,
+        piesCuadrados:"1,234",
+    },
+    {
+        foto:"https://photos.zillowstatic.com/fp/7da3205e09c1c4212c91fd79ac9adbc8-p_e.webp",
+        foto2:"https://photos.zillowstatic.com/fp/d8632262e98dfb9988abfc89997cd101-cc_ft_384.webp",
+        foto3:"https://photos.zillowstatic.com/fp/fdbc61d32a5aa6fef0b333c7eb5a78a0-cc_ft_384.webp",
+        foto4:"https://photos.zillowstatic.com/fp/3a8c12e258381a57e2c5a829fc1d268a-cc_ft_384.webp",
+        foto5:"https://photos.zillowstatic.com/fp/7da4fa1e79bc158e29df214c271e201f-cc_ft_384.webp",
+        precio:"$550,000",
+        direccion:"11004 SW 152nd Ter, Miami, FL 33157",
+        cuartos:6,
+        baños:4,
+        piesCuadrados:"1,334",
+    },
+    {
+        foto:"https://photos.zillowstatic.com/fp/6a7cb33973080f487e472fa8215020d4-p_e.webp",
+        foto2:"https://photos.zillowstatic.com/fp/fb4d196ad69a40ed6ccb041f6f8b1802-cc_ft_768.webp",
+        foto3:"https://photos.zillowstatic.com/fp/e18fd0952e89c1588afaa0f0e20c2b91-cc_ft_768.webp",
+        foto4:"https://photos.zillowstatic.com/fp/fa6966731a2e06da2f5f9cb026f5a940-cc_ft_768.webp",
+        foto5:"https://photos.zillowstatic.com/fp/b78c9edc9d8b3d557e63845b615af52e-cc_ft_384.webp",
+        precio:"$830,000",
+        direccion:"15711 SW 106th Ave, Miami, FL 33157",
+        cuartos:5,
+        baños:3,
+        piesCuadrados:"1,433",
+    },
+    {
+        foto:"https://photos.zillowstatic.com/fp/8af78d29b7c7c537bc2f48dcf11bf380-p_e.webp",
+        foto2:"https://photos.zillowstatic.com/fp/4029e9884abb989f8ad8850e2c346b4e-cc_ft_768.webp",
+        foto3:"https://photos.zillowstatic.com/fp/ae3a0f769c09328e3f294e022e3446f2-cc_ft_768.webp",
+        foto4:"https://photos.zillowstatic.com/fp/a5fa34a8d38e580d6ab23d1991ff969e-cc_ft_384.webp",
+        foto5:"https://photos.zillowstatic.com/fp/14af3027483e0ad380354e78d2dbc605-cc_ft_384.webp",
+        precio:"$320,000",
+        direccion:"10820 SW 165th St, Miami, FL 33157",
+        cuartos:6,
+        baños:4,
+        piesCuadrados:"1,255",
+    },
+    {
+        foto:"https://photos.zillowstatic.com/fp/e714213166f08b711f1919a1175f5c11-p_e.webp",
+        foto2:"https://photos.zillowstatic.com/fp/915b39b7ccb70c95341a10f02b300fae-cc_ft_384.webp",
+        foto3:"https://photos.zillowstatic.com/fp/1e0dd6ecd9823b6ba22b5871ff2de89a-cc_ft_384.webp",
+        foto4:"https://photos.zillowstatic.com/fp/225f83433ab5d40316d5d53bc0286de1-cc_ft_384.webp",
+        foto5:"https://photos.zillowstatic.com/fp/f13a01b62dbe5057b9584bdef9391df7-cc_ft_384.webp",
+        precio:"$500,000",
+        direccion:"15208 SW 112th Pl, Miami, FL 33157",
+        cuartos:3,
+        baños:3,
+        piesCuadrados:"1,234",
+    },
+    
+]
+
+$(document).ready(function(){
+    $(".contenedor-padre").hide()
+
+
+
+    for(let i=0; i<propiedad.length; i++){
+        $("#casas").append(
+            '<div class="casitas">'+
+            '<img src="' + propiedad[i].foto + '">'+
+            '<p>Precio: ' + propiedad[i].precio + '</p>'+
+            '<p>'+propiedad[i].cuartos+' bds | '+propiedad[i].baños+' ba | '+propiedad[i].piesCuadrados+' sqft - House for sale</p>'+
+            '<p>Dirección: '+ propiedad[i].direccion + '</p>'+
+            '</div>'
+        )
+    }
+
+    $(".casitas").click(function(){
+        let indice = $(this).index()
+        $("#precio").text(propiedad[indice].precio)
+        $("#direccion").text(propiedad[indice].direccion)
+        $("#habitaciones").text(propiedad[indice].cuartos)
+        $("#baño").text(propiedad[indice].baños)
+        $("#area").text(propiedad[indice].piesCuadrados)
+        $("#img1").attr("src", propiedad[indice].foto)
+        $("#img2").attr("src", propiedad[indice].foto2)
+        $("#img3").attr("src", propiedad[indice].foto3)
+        $("#img4").attr("src", propiedad[indice].foto4)
+        $("#img5").attr("src", propiedad[indice].foto5)
+
+        $("#popup").show()
+    })
+    $("#cerrar-popup").click(function(){
+        $("#popup").hide()
+    })
+
+    $("#vender").click(function(){
+        $("#abajo").css("transform", "rotate(180deg)")
+    })
+
+    $("#vender").dblclick(function(){
+        $("#abajo").css("transform", "rotate(360deg)")
+    })
+})
